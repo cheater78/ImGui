01ebf6429001b28819370b3fea6a9cc8f4edbdc1
cheater78 <75250245+cheater78@users.noreply.github.com>
2024-09-02 13:51:20 +0200

    premake

---
+++ premake5.lua
@@ -0,0 +1,51 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "on"
+	warnings "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+
+	filter "system:macosx"
+		pic "On"
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines 
+		{ 
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+	
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
